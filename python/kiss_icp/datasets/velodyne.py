c714c4bcd75bd59d6ff93375e02a3d44343fd0a1
Martin Valgur <martin.valgur@gmail.com>
2024-03-05 18:05:02 +0200

    Add Velodyne support, WIP

---
+++ python/kiss_icp/datasets/velodyne.py
@@ -0,0 +1,81 @@
+# MIT License
+#
+# Copyright (c) 2022 Ignacio Vizzo, Tiziano Guadagnino, Benedikt Mersch, Cyrill
+# Stachniss.
+# Copyright (c) 2024 Martin Valgur
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import os
+
+import numpy as np
+
+class VelodynePcapLoader:
+    """Velodyne pcap dataloader"""
+
+    def __init__(
+        self,
+        data_dir: str,
+        *_,
+        **__,
+    ):
+        try:
+            from velodyne_decoder import read_pcap
+        except ImportError as e:
+            if e.name == "velodyne_decoder":
+                print(f'velodyne_decoder is not installed on your system, run "pip install velodyne_decoder"')
+            else:
+                raise
+            exit(1)
+
+        assert os.path.isfile(data_dir), "Velodyne pcap dataloader expects an existing PCAP file"
+
+        # we expect `data_dir` param to be a path to the .pcap file, so rename for clarity
+        self._pcap_file = str(data_dir)
+
+        print("Pre-reading Velodyne pcap to count the scans number ...")
+        self._timestamps = np.array([stamp.device for stamp, _ in read_pcap(self._pcap_file)])
+        # self._timestamps = (self._timestamps - self._timestamps[0]) / (self._timestamps[-1] - self._timestamps[0])
+        print(f"Velodyne pcap total scans number:  {len(self._timestamps)}")
+
+        self._scans_iter = read_pcap(self._pcap_file)
+        self._next_idx = 0
+        self._timestamps = []
+
+    def __getitem__(self, idx):
+        assert self._next_idx == idx, (
+            "Velodyne pcap dataloader supports only sequential reads. "
+            f"Expected idx: {self._next_idx}, but got {idx}"
+        )
+        stamp, scan = next(self._scans_iter)
+        self._next_idx += 1
+
+        # Using device timestamp rather than host's, as it should be more accurate.
+        self._timestamps[self._next_idx - 1] = stamp.device
+
+        xyz = scan[:, :3]
+        timestamps = scan[:, 4]
+        timestamps = (timestamps - timestamps[0]) * (1 / (timestamps[-1] - timestamps[0]))
+        return xyz, timestamps
+
+    def get_frames_timestamps(self):
+        return self._timestamps
+
+    def __len__(self):
+        return len(self._timestamps)
